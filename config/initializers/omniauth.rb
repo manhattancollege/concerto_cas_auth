c96cd00e2c430ca637731335e9073e5029188947
August <august@inniogroup.com>
2014-07-31 21:33:17 -0400

    Set defaults in initial concerto config call

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,27 +1,16 @@
 # Check if our ConcertoConfig values have been created yet
 #   (this prevents issues with initializing omniauth-cas on first install)
-cas_config = ConcertoConfig.where(:category => "CAS User Authentication")
-if cas_config.length == 6 
-  # Store omniauth config values from main application's ConcertoConfig
-  omniauth_config = {
-    :host => ConcertoConfig[:cas_host],
-    :url => ConcertoConfig[:cas_url],
-    :uid_key => ConcertoConfig[:cas_uid_key],
-    :email_key => ConcertoConfig[:cas_email_key],
-    :first_name_key => ConcertoConfig[:cas_first_name_key],
-    :last_name_key => ConcertoConfig[:cas_last_name_key]
-  }
-else 
-  # Required ConcertoConfig keys have not been created, fallback to defaults
-  omniauth_config = {
-    :host => "cas.example.org",
-    :url => "https://cas.example.org/cas",
-    :uid_key => "user",
-    :email_key => "email",
-    :first_name_key => "first_name",
-    :last_name_key => "last_name"
-  }
-end
+
+
+# Store omniauth config values from main application's ConcertoConfig
+omniauth_config = {
+  :host => URI.parse(ConcertoConfig[:cas_url]).host,
+  :url => ConcertoConfig[:cas_url],
+  :uid_key => ConcertoConfig[:cas_uid_key],
+  :email_key => ConcertoConfig[:cas_email_key],
+  :first_name_key => ConcertoConfig[:cas_first_name_key],
+  :last_name_key => ConcertoConfig[:cas_last_name_key]
+}
 
 # configure omniauth-cas gem based on specified yml configs
 Rails.application.config.middleware.use OmniAuth::Builder do
