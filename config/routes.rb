0633d799f918774729c753376f337a58752fbb3b
August <august@inniogroup.com>
2014-10-26 21:08:53 -0400

    Remove duplicate routes that were breaking under rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
-Concerto::Application.routes.draw do
-  mount ConcertoCasAuth::Engine, :at => '/auth'
-end
-
 ConcertoCasAuth::Engine.routes.draw do
   get ":provider/callback", :to => "omniauth_callback#cas_auth"
 end
