057cce4f2286d02c4de7dcab0b6fec29d000457a
gabe283 <perez283@gmail.com>
2014-10-30 20:36:55 -0400

    Adds first_name_key, fixes issues with omniauth keys, and adds email suffix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,32 +7,38 @@ ConcertoConfig.make_concerto_config("cas_url", "https://cas.example.org/cas",
   :seq_no => 1, 
   :description =>"Defines the url of your CAS server")
 
-ConcertoConfig.make_concerto_config("cas_uid_key", "user", 
+ConcertoConfig.make_concerto_config("cas_uid_key", "uid", 
   :value_type => "string", 
-  :value_default => "user", 
   :category => "CAS User Authentication", 
   :seq_no => 2, 
   :description => "CAS field name containing user login names")
 
 ConcertoConfig.make_concerto_config("cas_email_key", "email", 
   :value_type => "string", 
-  :value_default => "email", 
   :category => "CAS User Authentication", 
   :seq_no => 3, 
   :description => "CAS field name containing user email addresses")
 
-ConcertoConfig.make_concerto_config("email_suffix", "@",
+ConcertoConfig.make_concerto_config("cas_email_suffix", "@",
   :value_type => "string",
   :category => "CAS User Authentication",
   :seq_no => 4,
   :description => "Appends this suffix to a CAS returned user id. Leave blank if using email_key above")
+
+ConcertoConfig.make_concerto_config("cas_first_name_key", "first_name",
+  :value_type => "string",
+  :category => "CAS User Authentication",
+  :seq_no => 5,
+  :description => "CAS field name containing first name")
 
 # Store omniauth config values from main application's ConcertoConfig
 omniauth_config = {
   :host => URI.parse(ConcertoConfig[:cas_url]).host,
   :url => ConcertoConfig[:cas_url],
   :uid_key => ConcertoConfig[:cas_uid_key],
+  :first_name_key => ConcertoConfig[:cas_first_name_key],
   :email_key => ConcertoConfig[:cas_email_key],
+  :email_suffix => ConcertoConfig[:cas_email_suffix],
   :callback_url => "/auth/cas/callback"
 }
 
